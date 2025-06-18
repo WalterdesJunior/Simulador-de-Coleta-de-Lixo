8153465ca6245e024134fc45ccc580f1b993bfb6
Walterdes Junior <168867428+WalterdesJunior@users.noreply.github.com>
2025-06-18 19:06:35 -0300

    Delete Principal.java

--- Principal.java
+++
@@ -1,7 +0,0 @@
-public class Principal {
-    public static void main(String[] args) {
-        Simulador simulador = new Simulador();
-        simulador.inicializar();
-        simulador.executarSimulacao(1440); // Simula 1 dia (1440 minutos)
-    }
-}
